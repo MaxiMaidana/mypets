03a9c3f7aa885b5c465b97ca9e06b0df8c7464b4
Maxi Maidana <maidana1454@gmail.com>
2023-01-27 12:12:46 -0300

    se agrega pagina con responsive tree

--- lib/feature/auth/presentation/page/auth_page.dart
+++ lib/feature/auth/presentation/page/auth_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/responsive/widget_tree.dart';
+
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text('login'),
+    return const Scaffold(
+      body: WidgetTree(
+        tiny: Text('tiny'),
+        phone: Text('phone'),
+        tablet: Text('tablet'),
+        largeTablet: Text('tablet large'),
+        computer: Text('computer'),
+      ),
     );
   }
 }
