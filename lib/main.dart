d788e2a73e47148ed752340d0e674c331b300eb6
Maxi Maidana <maidana1454@gmail.com>
2023-01-31 01:08:35 -0300

    se completa login con google en web

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
-      );
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await LocalStorage.configPrefs();
   setupLocator();
   runApp(const MyPetsApp());
