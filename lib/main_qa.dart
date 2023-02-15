735e4b283da2b68bcf41e1aad8b0c1f0c231603f
Maxi Maidana <maidana1454@gmail.com>
2023-02-15 13:07:27 -0300

    se agregan los flavors de qa, prod y dev

---
+++ lib/main_qa.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_utils/get_utils.dart';
+import 'package:mypets/firebase_options.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+import 'core/app/app.dart';
+import 'core/service/locator.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // if (GetPlatform.isWeb) {
+  //   await Firebase.initializeApp(
+  //     options: DefaultFirebaseOptions.currentPlatform,
+  //   );
+  // } else {
+  //   await Firebase.initializeApp();
+  // }
+  await Firebase.initializeApp();
+  await setupLocator();
+  usePathUrlStrategy();
+  runApp(const MyPetsApp());
+}
