3b42530a4bb7aae9b47ad283e410ace299d58bb3
Maxi <maidana1454@gmail.com>
2023-07-06 00:12:31 -0300

    se agrega equatable a petReminder

--- lib/feature/reminders/domain/model/pet_reminder.dart
+++ lib/feature/reminders/domain/model/pet_reminder.dart
@@ -1,9 +1,28 @@
+import 'package:equatable/equatable.dart';
 import 'package:googleapis/calendar/v3.dart';
 import 'package:mypets/data/models/pet/pet_model.dart';
 
-class PetReminder {
+class PetReminder extends Equatable {
   final PetModel petModel;
   final List<Event> lsEvents;
 
-  PetReminder({required this.petModel, required this.lsEvents});
+  const PetReminder({
+    required this.petModel,
+    required this.lsEvents,
+  });
+
+  PetReminder copyWith({
+    PetModel? petModel,
+    List<Event>? lsEvents,
+  }) =>
+      PetReminder(
+        petModel: petModel ?? this.petModel,
+        lsEvents: lsEvents ?? this.lsEvents,
+      );
+
+  @override
+  List<Object?> get props => [petModel, lsEvents];
+
+  @override
+  bool? get stringify => true;
 }
