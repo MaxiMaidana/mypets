f117d4650ab8e0ac25495db93fda83985cd9b907
Maxi <maidana1454@gmail.com>
2023-07-11 00:35:37 -0300

    fix error de delete reminder al ingresar y esta vencido

--- lib/feature/pets/presentation/getx/pets_controller.dart
+++ lib/feature/pets/presentation/getx/pets_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mypets/data/models/pet/pet_model.dart';
 import 'package:mypets/feature/app/presentation/getx/app_controller.dart';
@@ -34,15 +35,21 @@ class PetsController extends GetxController {
       isChargingPets.value = false;
       isSearchingReminders.value = true;
       for (var pet in petsLs) {
-        for (var id in pet.remindersId) {
-          final event = await _reminderController.getReminderData(id);
-          pet.lsEvents!.add(event);
+        if (pet.remindersId.isNotEmpty) {
+          for (var id in pet.remindersId) {
+            final eventRes = await _reminderController.getReminderData(id);
+            if (checkISValidDateTimeEvent(eventRes.end!.dateTime!)) {
+              pet.lsEvents!.add(eventRes);
+            } else {
+              log('entro por el else ${pet.name} con ${pet.lsEvents!.length}');
+              pet.lsEvents!.removeWhere((event) => event.id == eventRes.id);
+              await updatePet(pet.id!, pet);
+            }
+          }
         }
       }
       isSearchingReminders.value = false;
-      // _reminderController.page
       log('=========termino todo el tramite de mascotas=========');
-      // chargelsReminders
     } catch (e) {
       isChargingPets.value = false;
       rethrow;
@@ -59,6 +66,27 @@ class PetsController extends GetxController {
 
   PetModel searchPet(String id) =>
       petsLs.firstWhere((element) => element.id == id);
+
+  bool checkISValidDateTimeEvent(DateTime toEvaluate) {
+    DateTime now = DateTime.now();
+    if (toEvaluate.year > now.year) {
+      return true;
+    }
+    if (toEvaluate.day > now.day && toEvaluate.month <= now.month) {
+      return true;
+    }
+    if (toEvaluate.day < now.day && toEvaluate.month <= now.month) {
+      return false;
+    }
+    TimeOfDay timeNow = TimeOfDay.now();
+    if (timeNow.hour > toEvaluate.hour) {
+      return true;
+    }
+    if (timeNow.minute > toEvaluate.hour) {
+      return true;
+    }
+    return false;
+  }
 
   @override
   void onInit() {
